81b0f1b99783485d07d96a861e24f906ef684cd8
Ibrahim Merhi <ibrahim.m.merhy@gmail.com>
2026-08-28 07:45:00 +0000

    fix: enforce company in donor account search

--- accounting_custom/public/js/donor.js
+++ accounting_custom/public/js/donor.js
@@ -1,24 +1,31 @@
 frappe.ui.form.on("Donor", {
 	setup(frm) {
-		frm.set_query("account", "custom_accounts", (_doc, cdt, cdn) => {
-			const row = locals[cdt][cdn];
+		set_donor_account_queries(frm);
+	},
 
-			return {
-				filters: {
-					company: row.company,
-					is_group: 0,
-				},
-			};
-		});
+	refresh(frm) {
+		set_donor_account_queries(frm);
 	},
 });
 
 frappe.ui.form.on("Party Account", {
 	company(frm, cdt, cdn) {
-		const row = locals[cdt][cdn];
-
-		if (row.account) {
-			frappe.model.set_value(cdt, cdn, "account", null);
-		}
+		frappe.model.set_value(cdt, cdn, "account", null);
+		frappe.model.set_value(cdt, cdn, "advance_account", null);
 	},
 });
+
+function set_donor_account_queries(frm) {
+	["account", "advance_account"].forEach((fieldname) => {
+		frm.set_query(fieldname, "custom_accounts", (_doc, cdt, cdn) => {
+			const row = locals[cdt][cdn];
+
+			return {
+				query: "erpnext.controllers.queries.get_account_list",
+				filters: row.company
+					? { company: row.company, disabled: 0, is_group: 0 }
+					: { name: ["=", ""] },
+			};
+		});
+	});
+}
