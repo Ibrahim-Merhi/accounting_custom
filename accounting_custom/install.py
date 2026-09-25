eee079bfaad4d1ce8226409dde2e3e67ec7be107
Ibrahim Merhi <ibrahim.m.merhy@gmail.com>
2026-09-25 08:49:43 +0000

    Revert "Add HR manager workspace dashboard"
    This reverts commit 6c2374b438dedc27fbf512424b9fca64944d5b26.

--- accounting_custom/install.py
+++ accounting_custom/install.py
@@ -11,7 +11,6 @@ from accounting_custom.setup.metadata import ensure_visible_metadata
 from accounting_custom.setup.print_formats import ensure_arabic_voucher_print_formats
 from accounting_custom.setup.workspace import (
 	ensure_accounting_workspace_sections,
-	ensure_hr_manager_workspace,
 	remove_standalone_accounting_program_workspace,
 )
 
@@ -47,7 +46,6 @@ def setup_accounting_customizations():
 	backfill_arabic_amounts()
 	backfill_journal_entry_transaction_currency()
 	ensure_accounting_workspace_sections()
-	ensure_hr_manager_workspace()
 	remove_standalone_accounting_program_workspace()
 
 
